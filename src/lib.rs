d1a25f2d0afe1dcf26e78b2e14cad658639632b1
Giacomo Bagnoli <gbagnoli@gmail.com>
2021-03-28 15:45:29 +0100

    Add commandline parsing for database url

--- src/lib.rs
+++ src/lib.rs
@@ -1,15 +1,22 @@
 #[macro_use]
 extern crate diesel;
-extern crate dotenv;
+extern crate clap;
 
 pub mod models;
 pub mod schema;
 
 use crate::diesel::Connection;
 use diesel::sqlite::SqliteConnection;
+use clap::App;
 
-pub fn establish_connection() -> SqliteConnection {
-    dotenv::dotenv().ok();
-    let database_url = dotenv::var("DATABASE_URL").expect("DATABASE_URL env var must be set");
-    SqliteConnection::establish(&database_url).expect("cannot open sqlite database")
+pub fn establish_connection(database_url: std::string::String) -> SqliteConnection {
+    println!("Connecting to sqlite database at {}", database_url);
+    let conn = SqliteConnection::establish(&database_url).expect("cannot open sqlite database");
+    conn.execute("PRAGMA foreign_keys = ON").expect("Error trying to enable foreign keys");
+    conn
+}
+
+pub fn parse_args() -> clap::ArgMatches {
+    let config_yaml = clap::load_yaml!("args.yaml");
+    App::from(config_yaml).version(clap::crate_version!()).get_matches()
 }
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
+extern crate dotenv;
+
 fn main() {
-    println!("Hello, world!");
+    dotenv::dotenv().ok();
+    let database_env_url = dotenv::var("DATABASE_URL");
+    let matches = vpnutils::parse_args();
+    let database_url = match matches.value_of("database") {
+        Some(u) => String::from(u),
+        None => database_env_url.expect("no database url set on commandline or env DATABASE_URL")
+    };
+    vpnutils::establish_connection(database_url);
 }
