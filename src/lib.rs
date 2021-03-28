adcef84c22c440019c3ed446b9b6dc5786a4b3a9
Giacomo Bagnoli <gbagnoli@gmail.com>
2021-03-29 00:19:45 +0100

    rustfmt

--- src/lib.rs
+++ src/lib.rs
@@ -6,17 +6,20 @@ pub mod models;
 pub mod schema;
 
 use crate::diesel::Connection;
-use diesel::sqlite::SqliteConnection;
 use clap::App;
+use diesel::sqlite::SqliteConnection;
 
 pub fn establish_connection(database_url: std::string::String) -> SqliteConnection {
     println!("Connecting to sqlite database at {}", database_url);
     let conn = SqliteConnection::establish(&database_url).expect("cannot open sqlite database");
-    conn.execute("PRAGMA foreign_keys = ON").expect("Error trying to enable foreign keys");
+    conn.execute("PRAGMA foreign_keys = ON")
+        .expect("Error trying to enable foreign keys");
     conn
 }
 
 pub fn parse_args() -> clap::ArgMatches {
     let config_yaml = clap::load_yaml!("args.yaml");
-    App::from(config_yaml).version(clap::crate_version!()).get_matches()
+    App::from(config_yaml)
+        .version(clap::crate_version!())
+        .get_matches()
 }
--- src/main.rs
+++ src/main.rs
@@ -6,7 +6,7 @@ fn main() {
     let matches = vpnutils::parse_args();
     let database_url = match matches.value_of("database") {
         Some(u) => String::from(u),
-        None => database_env_url.expect("no database url set on commandline or env DATABASE_URL")
+        None => database_env_url.expect("no database url set on commandline or env DATABASE_URL"),
     };
     vpnutils::establish_connection(database_url);
 }
