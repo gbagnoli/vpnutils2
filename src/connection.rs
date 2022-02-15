d99846f62caa380c7ae9b232c93c62e4f59967c3
Giacomo Bagnoli <gbagnoli@gmail.com>
2022-02-15 15:27:55 +0000

    Implement decrypt, encrypt backup for database connection

--- src/connection.rs
+++ src/connection.rs
@@ -5,45 +5,54 @@ use diesel::Connection;
 use std::fs::File;
 use std::io::{Read, Write};
 
+embed_migrations!();
+
 pub struct Database {
     directory: tempfile::TempDir,
     database_path: String,
     backup_path: String,
-    source: std::path::PathBuf,
+    source_path: String,
     password: String,
+}
+
+fn path_to_string(path: &std::path::PathBuf) -> Result<String> {
+    match path.as_path().to_str().map(|s| s.to_string()) {
+        None => Err(anyhow::anyhow!("Cannot convert path UTF-8")),
+        Some(x) => Ok(x),
+    }
 }
 
 impl Database {
     pub fn create(source: std::path::PathBuf, password: String) -> Result<Self> {
+        let db = Self::new(source, password)?;
         // need to create a new file with diesel setup
         // then move it to the temp directory, and save it
-        Self::open(source, password)
+        let conn = SqliteConnection::establish(&db.database_path)?;
+        println!("Running migrations on new db at {}", db.database_path);
+        embedded_migrations::run(&conn)?;
+        db.save()?;
+        Ok(db)
     }
 
     pub fn open(source: std::path::PathBuf, password: String) -> Result<Self> {
+        let db = Self::new(source, password)?;
+        db.decrypt()?;
+        embedded_migrations::run(&db.connect()?)?;
+        Ok(db)
+    }
+
+    fn new(source: std::path::PathBuf, password: String) -> Result<Self> {
         let dir = tempfile::tempdir()?;
-        let db_path = dir
-            .path()
-            .join("database.db")
-            .into_os_string()
-            .into_string()
-            .expect("cannot decode database url");
-
-        let backup_path = dir
-            .path()
-            .join("backup.db")
-            .into_os_string()
-            .into_string()
-            .expect("cannot decode database url");
-
+        let db_path = path_to_string(&dir.path().join("database.db"))?;
+        let backup_path = path_to_string(&dir.path().join("backup.db"))?;
+        let source_path = path_to_string(&source)?;
         let db = Database {
             directory: dir,
             database_path: db_path,
             backup_path,
-            source,
+            source_path,
             password,
         };
-        db.decrypt()?;
         Ok(db)
     }
 
@@ -61,27 +70,48 @@ impl Database {
     }
 
     fn encrypt(&self) -> Result<()> {
-        let mut input = File::open(&self.backup_path)?;
-        let output = File::open(&self.source)?;
+        let mut input = File::open(&self.backup_path)
+            .with_context(|| format!("Trying to open the backup file {}", self.backup_path))?;
+        let output = File::create(&self.source_path)
+            .with_context(|| format!("Trying to open the source file {}", self.source_path))?;
+
+        println!("Encrypting database to {}", self.source_path);
         let mut buffer = vec![];
         let encryptor = age::Encryptor::with_user_passphrase(Secret::new(self.password.to_owned()));
-        let mut writer = encryptor.wrap_output(output)?;
+        let mut writer = encryptor
+            .wrap_output(output)
+            .with_context(|| format!("Setting up the encryptor for {}", self.source_path))?;
         input.read_to_end(&mut buffer)?;
         writer.write(&buffer[..])?;
         writer.finish()?;
+        std::fs::remove_file(&self.backup_path)?;
         Ok(())
     }
 
     fn decrypt(&self) -> Result<()> {
-        let input = File::open(&self.source)?;
-        let mut output = File::open(&self.database_path)?;
-        let decryptor = match age::Decryptor::new(&input)? {
+        let input = File::open(&self.source_path)
+            .with_context(|| format!("Trying to open the source file {}", self.source_path))?;
+        let mut output = File::create(&self.database_path).with_context(|| {
+            format!(
+                "Trying to create the destination file {}",
+                self.database_path
+            )
+        })?;
+        println!("Decrypting database from {}", self.source_path);
+        let decryptor = match age::Decryptor::new(&input).with_context(|| {
+            format!(
+                "While setting up decryptor for file at {}",
+                self.source_path
+            )
+        })? {
             age::Decryptor::Passphrase(d) => d,
             _ => unreachable!(),
         };
         let mut reader = decryptor.decrypt(&Secret::new(self.password.to_owned()), None)?;
         let mut buffer = vec![];
-        reader.read_to_end(&mut buffer)?;
+        reader
+            .read_to_end(&mut buffer)
+            .with_context(|| format!("Cannot read source file at {}", self.source_path))?;
         output.write(&buffer[..])?;
         Ok(())
     }
@@ -89,8 +119,10 @@ impl Database {
     fn backup(&self) -> Result<()> {
         let conn = self.connect()?;
         // this is an alternative to the backup API https://www.sqlite.org/lang_vacuum.html#vacuuminto
-        conn.execute(&format!("VACUUM INTO {}", self.backup_path))
-            .context("Error while calling VACUUM")?;
+        let sql = format!("VACUUM main INTO '{}'", self.backup_path);
+        println!("Saving database to {}", self.backup_path);
+        conn.execute(&sql)
+            .with_context(|| format!("Error while calling VACUUM: {}", sql))?;
         Ok(())
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,25 @@
 extern crate dotenv;
+use anyhow::Result;
 use clap::Parser;
 use dialoguer::{theme::ColorfulTheme, Password};
 
-fn main() {
+fn main() -> Result<()> {
     dotenv::dotenv().ok();
     let args = vpnutils::Cli::parse();
-    let password = match Password::with_theme(&ColorfulTheme::default())
+    let password = Password::with_theme(&ColorfulTheme::default())
         .with_prompt("Database Password")
-        .interact()
-    {
-        Err(_e) => {
-            println!("Failed to read password");
-            std::process::exit(1);
-        }
-        Ok(p) => p,
-    };
-    let db = vpnutils::Database::open(args.database_path, password).unwrap();
-    println!("Connecting to database");
-    db.connect().unwrap();
+        .interact()?;
 
-    db.save().unwrap();
+    // let db = vpnutils::Database::create(args.database_path, password)?;
+    match vpnutils::Database::open(args.database_path.clone(), password.clone()) {
+        Ok(db) => {
+            println!("Connecting to database");
+            db.connect()?;
+            db.save()
+        }
+        Err(_) => {
+            vpnutils::Database::create(args.database_path, password)?;
+            Ok(())
+        }
+    }
 }
