00fa9fc5976636a05b158270c8e68e914fd50362
Giacomo Bagnoli <gbagnoli@gmail.com>
2022-03-05 16:13:10 +0000

    Add some more checks

--- tests/integration_tests.rs
+++ tests/integration_tests.rs
@@ -3,16 +3,24 @@ use anyhow::Result;
 static PASSWORD: &str = "supersafe";
 
 #[test]
-fn test_create() -> Result<()> {
+fn test_database() -> Result<()> {
     let dir = tempfile::tempdir()?;
     let db_path = dir.path().join("database.db");
     let db_str = db_path.into_os_string().into_string().unwrap();
+
+    // file does not exists
+    assert!(vpnutils::Database::open(&db_str, String::from("otherpass")).is_err());
+
+    // I can create, connect, and save back
     let db = vpnutils::Database::create(&db_str, PASSWORD.to_string())?;
-
-    assert!(vpnutils::Database::create(&db_str, PASSWORD.to_string()).is_err());
-
     db.connect()?;
     db.save()?;
+
+    // cannot create twice
+    assert!(vpnutils::Database::create(&db_str, PASSWORD.to_string()).is_err());
+
+    // password mismatch
+    assert!(vpnutils::Database::open(&db_str, String::from("otherpass")).is_err());
 
     let new = vpnutils::Database::open(&db_str, PASSWORD.to_string())?;
     assert_eq!(db_str, db.path());
